7e5f7f082efc6bb5de4165ee729d15dc8bb3122f
KitK <147215922+Kitk00@users.noreply.github.com>
2025-02-23 18:09:21 -0500

    added simple enemy ai
    enemy follows player

---
+++ Cadabra/Assets/Cadabra/Enemy/EnemyRefrences.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace GDL_Tutorial
+{
+    [DisallowMultipleComponent]
+    public class EnemyRefrences : MonoBehaviour
+    {
+        [HideInInspector] public NavMeshAgent navMeshagent;
+        [HideInInspector] public Animator animator;
+
+        [Header("Stats")]
+
+        public float pathUpdateDelay = 0.2f;
+
+        private void Awake()
+        {
+            navMeshagent = GetComponent<NavMeshAgent>();
+            animator = GetComponent<Animator>();
+        }
+    }
+}
---
+++ Cadabra/Assets/Cadabra/Enemy/EnemySimple.cs
@@ -0,0 +1,67 @@
+using Codice.Client.Common.GameUI;
+using GDL_Tutorial;
+using PlasticPipe.PlasticProtocol.Messages;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySimple : MonoBehaviour
+{
+    public Transform target;
+
+    private EnemyRefrences enemyRefrences;
+
+    private float pathUpdateDeadline;
+
+    private float shootingDistance;
+
+    private void Awake()
+    {
+        enemyRefrences = GetComponent<EnemyRefrences>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shootingDistance = enemyRefrences.navMeshagent.stoppingDistance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target != null)
+        {
+            bool inRange = Vector3.Distance(transform.position, target.position) <= shootingDistance;
+            
+            if(inRange)
+            {
+                LookAtTarget();
+            }
+            else
+            {
+                UpdatePath();
+            }
+
+            //enemyRefrences.animator.SetBool("shooting", inRange);
+        }
+        enemyRefrences.animator.SetFloat("speed", enemyRefrences.navMeshagent.desiredVelocity.sqrMagnitude);
+    }
+
+    private void LookAtTarget()
+    {
+        Vector3 lookPos = target.position - transform.position;
+        lookPos.y = 0;
+        Quaternion rotation = Quaternion.LookRotation(lookPos);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 0.2f);
+    }
+
+    private void UpdatePath()
+    {
+        if(Time.time >= pathUpdateDeadline)
+        {
+            Debug.Log("Updating Path");
+            pathUpdateDeadline = Time.time + enemyRefrences.pathUpdateDelay;
+            enemyRefrences.navMeshagent.SetDestination(target.position);
+        }
+    }
+}
