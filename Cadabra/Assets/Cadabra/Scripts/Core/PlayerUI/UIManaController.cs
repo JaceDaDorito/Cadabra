1f23eae9ef400a02092ad54cce2a928a2756dd2d
argetlam <marrislucas@gmail.com>
2025-03-09 16:30:49 -0400

    UI temp fix

--- Cadabra/Assets/Cadabra/Scripts/Core/PlayerUI/UIManaController.cs
+++ Cadabra/Assets/Cadabra/Scripts/Core/PlayerUI/UIManaController.cs
@@ -15,18 +15,18 @@ public class UIManaController : MonoBehaviour
     // Inherited
     private void Start()
     {
-        maxPosX = _manaForeground.localPosition.x;
-        maxPosY = _manaForeground.localPosition.y;
-        maxPosZ = _manaForeground.localPosition.z;
-        maxScaleX = _manaForeground.localScale.x;
-        maxScaleY = _manaForeground.localScale.y;
-        maxScaleZ = _manaForeground.localScale.z;
+        maxPosX = -50;
+        maxPosY = -150;
+        maxPosZ = 0;
+        maxScaleX = 70;
+        maxScaleY = 70;
+        maxScaleZ = 1;
     }
 
     public void UpdateMana(float currentMana, float maxMana)
     {
         _manaText.text = "Mana: " + currentMana + "/" + maxMana;
         _manaForeground.localScale = new Vector3(maxScaleX, maxScaleY*(currentMana/maxMana), maxScaleZ);
-        _manaForeground.localPosition = new Vector3(maxPosX, maxPosY - (0.5f*(maxScaleY*((maxMana-currentMana)/maxMana))), maxScaleZ);
+        _manaForeground.localPosition = new Vector3(maxPosX, maxPosY - (0.5f*(maxScaleY*((maxMana-currentMana)/maxMana))), maxPosZ);
     }
 }
